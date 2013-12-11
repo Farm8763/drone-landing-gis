e702817d2a9d8e48dc3604e3dddc776ff3c91bc1
ryan <rmcneely@teksavvy.com>
2013-12-11 17:40:14 -0500

    More unsupervised classification code

--- src/unsupervised_classification.py
+++ src/unsupervised_classification.py
@@ -1,7 +1,18 @@
 from spectral import *
+import pylab
 
 class unclass:
-    def perform_clustering(self, file, numClusters, numIterations):
-        img = open_image(file).load()
-        (m, c) = kmeans(img, numClusters, numIterations)
+    def __init__(self, numClusters, numIterations):
+        self.numClusters = numClusters
+        self.numIterations = numIterations
         
+    def perform_clustering(self, file):
+        img = open_image(file).load()
+        (self.map, self.clusters) = kmeans(img, self.numClusters, self.numIterations)
+        
+    def displayMap(self):
+        pylab.figure()
+        pylab.hold(1)
+        for i in range(self.clusters.shape[0]):
+            pylab.plot(self.clusters[i])
+        pylab.show()
