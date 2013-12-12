1f393082f2941978c66e56bfdf13734b96b810d8
ryan <rmcneely@teksavvy.com>
2013-12-11 23:20:56 -0500

    Updated

--- test/unsupervised_classification_test.py
+++ test/unsupervised_classification_test.py
@@ -1,6 +1,9 @@
-from drone-landing-gis import unsupervised_classification as uc
+import unsupervised_classification as uc
+import subprocess
 
-fileName = ""
+tifFilePath = ""
+subprocess.call(['test.sh', tifFilePath])
+fileName = tifFilePath + ".lan"
 filePath = "test/data/" + fileName
 myUnclass = uc.unclass(5, 10)
 myUnclass.perform_clustering(filePath)
