c8aee57af4b4a38daa3ef7cfaa65a5cdb9409486
ryan <ryan@ubuntu-mobile>
2013-12-13 21:00:39 -0500

    working convert

--- src/convertGeoTifToLan.sh
+++ src/convertGeoTifToLan.sh
@@ -1,2 +1,2 @@
-echo $1
+#!/bin/bash
 gdal_translate -of LAN $1.tif $1.lan
--- test/unsupervised_classification_test.py
+++ test/unsupervised_classification_test.py
@@ -4,6 +4,8 @@ import subprocess
 
 projectDirectory = '/home/ryan/geoworkspace/git/gis-files/'
 tifFilePath = projectDirectory + 'test/data/015029_0100_991101_l7_743_lcc00'
+print projectDirectory + 'src/convertGeoTifToLan.sh'
+print tifFilePath
 subprocess.call([projectDirectory + 'src/convertGeoTifToLan.sh', tifFilePath])
 #lanFileName = tifFilePath + ".lan"
 #lanFilePath = projectDirectory + "test/data/" + lanFileName
